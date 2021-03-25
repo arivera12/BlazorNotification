dcc9d33407c9194dbf85ce56651b7d21e49a390d
Anthony Rivera <anthony_elgueta@hotmail.com>
2021-03-24 22:38:25 -0400

    Removed plurality.

--- BlazorNotification/Extensions/NotificationExtensions.cs
+++ BlazorNotification/Extensions/NotificationExtensions.cs
@@ -4,7 +4,7 @@ namespace BlazorNotifications
 {
     public static class NotificationExtensions
     {
-        public static IServiceCollection AddNotifications(this IServiceCollection services)
+        public static IServiceCollection AddBlazorNotification(this IServiceCollection services)
         {
             return services.AddScoped<IBlazorNotificationService, BlazorNotificationService>();
         }
